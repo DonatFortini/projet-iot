e34e9f9007190444fb767df56b260380b317bdcf
DonatFortini <donatfortini2a@gmail.com>
2024-04-16 00:01:34 +0200

    refactor code moteur

---
+++ projet_top_secret.ino
@@ -0,0 +1,60 @@
+#include <Servo.h>
+
+Servo servoBot;
+Servo servoTop;
+
+const int maxStep = 4;
+
+int data[2] = {90, 90};
+int last_data[2] = {90, 90};
+
+void setup()
+{
+  servoBot.attach(10);
+  servoTop.attach(9);
+
+  servoBot.write(data[0]);
+  servoTop.write(data[1]);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  receivedData(data);
+
+  if (data[0] != last_data[0] || data[1] != last_data[1])
+  {
+    servoBot.write(data[0]);
+    servoTop.write(data[1]);
+
+    last_data[0] = data[0];
+    last_data[1] = data[1];
+  }
+
+  delay(100);
+}
+
+void receivedData(int *data)
+{
+  if (Serial.available() > 0)
+  {
+    char receivedChars[32];
+    Serial.readBytesUntil('\n', receivedChars, sizeof(receivedChars));
+    int number1, number2;
+    int fieldsRead = sscanf(receivedChars, "%d,%d", &number1, &number2);
+
+    if (fieldsRead == 2)
+    {
+      Serial.print("Entier 1: ");
+      Serial.println(number1);
+      Serial.print("Entier 2: ");
+      Serial.println(number2);
+      data[0] = number1;
+      data[1] = number2;
+    }
+    else
+    {
+      Serial.println("Erreur: format non valide. Utilisez le format 'entier1,entier2'.");
+    }
+  }
+}
