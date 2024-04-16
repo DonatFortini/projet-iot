4a11eb5267f2c9a738365ddcd2ab55f3b1aaf33a
DonatFortini <donatfortini2a@gmail.com>
2024-04-16 23:49:02 +0200

    test fusion 1ere partie

--- esp-32-camera.ino
+++ esp-32-camera.ino
@@ -1,88 +1,47 @@
-/* Edge Impulse Arduino examples
- * Copyright (c) 2022 EdgeImpulse Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 /* Includes ---------------------------------------------------------------- */
 #include <iot_tracking_inferencing.h>
 #include "edge-impulse-sdk/dsp/image/image.hpp"
-
 #include "esp_camera.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
 
-// Select camera model - find more camera models in camera_pins.h file here
-// https://github.com/espressif/arduino-esp32/blob/master/libraries/ESP32/examples/Camera/CameraWebServer/camera_pins.h
-
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+/*Define  module and pins*/
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    4
-#define SIOD_GPIO_NUM    18
-#define SIOC_GPIO_NUM    23
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
 
-#define Y9_GPIO_NUM      36
-#define Y8_GPIO_NUM      37
-#define Y7_GPIO_NUM      38
-#define Y6_GPIO_NUM      39
-#define Y5_GPIO_NUM      35
-#define Y4_GPIO_NUM      14
-#define Y3_GPIO_NUM      13
-#define Y2_GPIO_NUM      34
-#define VSYNC_GPIO_NUM   5
-#define HREF_GPIO_NUM    27
-#define PCLK_GPIO_NUM    25
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
-
-#else
-#error "Camera model not selected"
-#endif
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 /* Constant defines -------------------------------------------------------- */
-#define EI_CAMERA_RAW_FRAME_BUFFER_COLS           320
-#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           240
-#define EI_CAMERA_FRAME_BYTE_SIZE                 3
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS 320
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS 240
+#define EI_CAMERA_FRAME_BYTE_SIZE 3
 
 /* Private variables ------------------------------------------------------- */
 static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
 static bool is_initialised = false;
-uint8_t *snapshot_buf; //points to the output of the capture
+uint8_t *snapshot_buf; // points to the output of the capture
+WiFiServer server(80);
+const char *ssid = "Airbox-0FDB";
+const char *password = "csm27LnEa9VX";
+
+StaticJsonDocument<250> jsonDocument;
+char buffer[250];
 
 static camera_config_t camera_config = {
     .pin_pwdn = PWDN_GPIO_NUM,
@@ -103,63 +62,76 @@ static camera_config_t camera_config = {
     .pin_href = HREF_GPIO_NUM,
     .pin_pclk = PCLK_GPIO_NUM,
 
-    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
     .xclk_freq_hz = 20000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 
-    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+    .pixel_format = PIXFORMAT_JPEG, // YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_QVGA,   // QQVGA-UXGA Do not use sizes above QVGA when not JPEG
 
-    .jpeg_quality = 12, //0-63 lower number means higher quality
-    .fb_count = 1,       //if more than one, i2s runs in continuous mode. Use only with JPEG
+    .jpeg_quality = 12, // 0-63 lower number means higher quality
+    .fb_count = 1,      // if more than one, i2s runs in continuous mode. Use only with JPEG
     .fb_location = CAMERA_FB_IN_PSRAM,
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
 /* Function definitions ------------------------------------------------------- */
+/*ML Functions*/
 bool ei_camera_init(void);
 void ei_camera_deinit(void);
-bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) ;
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf);
+
+/*Server Functions*/
+void wifi_connection(void);
+void start_server(void);
+
+void getData(void);
+void create_json(char *tag, float value, char *unit);
+void add_json_object(char *tag, float value, char *unit);
 
 /**
-* @brief      Arduino setup function
-*/
+ * @brief      Arduino setup function
+ */
 void setup()
 {
-    // put your setup code here, to run once:
     Serial.begin(115200);
-    //comment out the below line to start inference immediately after upload
-    while (!Serial);
-    Serial.println("Edge Impulse Inferencing Demo");
-    if (ei_camera_init() == false) {
+    // comment out the below line to start inference immediately after upload
+    while (!Serial)
+        ;
+
+    wifi_connection();
+    start_server();
+    ei_sleep(5000);
+
+    if (!ei_camera_init())
         ei_printf("Failed to initialize Camera!\r\n");
-    }
-    else {
+    else
         ei_printf("Camera initialized\r\n");
-    }
 
     ei_printf("\nStarting continious inference in 2 seconds...\n");
     ei_sleep(2000);
 }
 
 /**
-* @brief      Get data and run inferencing
-*
-* @param[in]  debug  Get debug info if true
-*/
+ * @brief      Get data and run inferencing
+ *
+ * @param[in]  debug  Get debug info if true
+ */
 void loop()
 {
-
+    server.handleClient();
     // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
-    if (ei_sleep(5) != EI_IMPULSE_OK) {
+    if (ei_sleep(5) != EI_IMPULSE_OK)
+    {
         return;
     }
 
-    snapshot_buf = (uint8_t*)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+    snapshot_buf = (uint8_t *)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
 
     // check if allocation was successful
-    if(snapshot_buf == nullptr) {
+    if (snapshot_buf == nullptr)
+    {
         ei_printf("ERR: Failed to allocate snapshot buffer!\n");
         return;
     }
@@ -168,51 +140,108 @@ void loop()
     signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
     signal.get_data = &ei_camera_get_data;
 
-    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false) {
+    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false)
+    {
         ei_printf("Failed to capture image\r\n");
         free(snapshot_buf);
         return;
     }
 
     // Run the classifier
-    ei_impulse_result_t result = { 0 };
+    ei_impulse_result_t result = {0};
 
     EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
-    if (err != EI_IMPULSE_OK) {
+    if (err != EI_IMPULSE_OK)
+    {
         ei_printf("ERR: Failed to run classifier (%d)\n", err);
         return;
     }
 
     // print the predictions
     ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
-                result.timing.dsp, result.timing.classification, result.timing.anomaly);
+              result.timing.dsp, result.timing.classification, result.timing.anomaly);
 
 #if EI_CLASSIFIER_OBJECT_DETECTION == 1
     bool bb_found = result.bounding_boxes[0].value > 0;
-    for (size_t ix = 0; ix < result.bounding_boxes_count; ix++) {
+    for (size_t ix = 0; ix < result.bounding_boxes_count; ix++)
+    {
         auto bb = result.bounding_boxes[ix];
-        if (bb.value == 0) {
+        if (bb.value == 0)
+        {
             continue;
         }
         ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
     }
-    if (!bb_found) {
+    if (!bb_found)
+    {
         ei_printf("    No objects found\n");
     }
 #else
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
+    {
         ei_printf("    %s: %.5f\n", result.classification[ix].label,
-                                    result.classification[ix].value);
+                  result.classification[ix].value);
     }
 #endif
 
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
-        ei_printf("    anomaly score: %.3f\n", result.anomaly);
+    ei_printf("    anomaly score: %.3f\n", result.anomaly);
 #endif
 
-
     free(snapshot_buf);
+}
 
+/**
+ * @brief      Connecte le device a internet
+ *
+ */
+void wifi_connection(void)
+{
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Connecting...");
+    }
+    Serial.println("Connected to WiFi");
+}
+
+/**
+ * @brief      Demmarre le server
+ *
+ */
+void start_server(void)
+{
+    server.begin();
+    Serial.print("Connected to wifi. My address:");
+    IPAddress myAddress = WiFi.localIP();
+    Serial.println(myAddress);
+    server.on("/data", getData);
+}
+
+void create_json(char *tag, float value, char *unit)
+{
+    jsonDocument.clear();
+    jsonDocument["type"] = tag;
+    jsonDocument["value"] = value;
+    jsonDocument["unit"] = unit;
+    serializeJson(jsonDocument, buffer);
+}
+
+void add_json_object(char *tag, float value, char *unit)
+{
+    JsonObject obj = jsonDocument.createNestedObject();
+    obj["type"] = tag;
+    obj["value"] = value;
+    obj["unit"] = unit;
+}
+
+void getData(void)
+{
+    Serial.println("Get data");
+    create_json("name", value, "param");
+    server.send(200, "application/json", buffer);
 }
 
 /**
@@ -220,39 +249,26 @@ void loop()
  *
  * @retval  false if initialisation failed
  */
-bool ei_camera_init(void) {
-
-    if (is_initialised) return true;
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
-    //initialize the camera
+bool ei_camera_init(void)
+{
+    if (is_initialised)
+        return true;
+    // initialize the camera
     esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK) {
-      Serial.printf("Camera init failed with error 0x%x\n", err);
-      return false;
+    if (err != ESP_OK)
+    {
+        Serial.printf("Camera init failed with error 0x%x\n", err);
+        return false;
     }
 
-    sensor_t * s = esp_camera_sensor_get();
+    sensor_t *s = esp_camera_sensor_get();
     // initial sensors are flipped vertically and colors are a bit saturated
-    if (s->id.PID == OV3660_PID) {
-      s->set_vflip(s, 1); // flip it back
-      s->set_brightness(s, 1); // up the brightness just a bit
-      s->set_saturation(s, 0); // lower the saturation
+    if (s->id.PID == OV3660_PID)
+    {
+        s->set_vflip(s, 1);      // flip it back
+        s->set_brightness(s, 1); // up the brightness just a bit
+        s->set_saturation(s, 0); // lower the saturation
     }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE)
-    s->set_vflip(s, 1);
-    s->set_hmirror(s, 1);
-#elif defined(CAMERA_MODEL_ESP_EYE)
-    s->set_vflip(s, 1);
-    s->set_hmirror(s, 1);
-    s->set_awb_gain(s, 1);
-#endif
-
     is_initialised = true;
     return true;
 }
@@ -260,9 +276,9 @@ bool ei_camera_init(void) {
 /**
  * @brief      Stop streaming of sensor data
  */
-void ei_camera_deinit(void) {
-
-    //deinitialize the camera
+void ei_camera_deinit(void)
+{
+    // deinitialize the camera
     esp_err_t err = esp_camera_deinit();
 
     if (err != ESP_OK)
@@ -275,7 +291,6 @@ void ei_camera_deinit(void) {
     return;
 }
 
-
 /**
  * @brief      Capture, rescale and crop image
  *
@@ -287,45 +302,49 @@ void ei_camera_deinit(void) {
  * @retval     false if not initialised, image captured, rescaled or cropped failed
  *
  */
-bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) {
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf)
+{
     bool do_resize = false;
 
-    if (!is_initialised) {
+    if (!is_initialised)
+    {
         ei_printf("ERR: Camera is not initialized\r\n");
         return false;
     }
 
     camera_fb_t *fb = esp_camera_fb_get();
 
-    if (!fb) {
+    if (!fb)
+    {
         ei_printf("Camera capture failed\n");
         return false;
     }
 
-   bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_JPEG, snapshot_buf);
+    bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_JPEG, snapshot_buf);
 
-   esp_camera_fb_return(fb);
+    esp_camera_fb_return(fb);
 
-   if(!converted){
-       ei_printf("Conversion failed\n");
-       return false;
-   }
+    if (!converted)
+    {
+        ei_printf("Conversion failed\n");
+        return false;
+    }
 
-    if ((img_width != EI_CAMERA_RAW_FRAME_BUFFER_COLS)
-        || (img_height != EI_CAMERA_RAW_FRAME_BUFFER_ROWS)) {
+    if ((img_width != EI_CAMERA_RAW_FRAME_BUFFER_COLS) || (img_height != EI_CAMERA_RAW_FRAME_BUFFER_ROWS))
+    {
         do_resize = true;
     }
 
-    if (do_resize) {
+    if (do_resize)
+    {
         ei::image::processing::crop_and_interpolate_rgb888(
-        out_buf,
-        EI_CAMERA_RAW_FRAME_BUFFER_COLS,
-        EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
-        out_buf,
-        img_width,
-        img_height);
+            out_buf,
+            EI_CAMERA_RAW_FRAME_BUFFER_COLS,
+            EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
+            out_buf,
+            img_width,
+            img_height);
     }
-
 
     return true;
 }
@@ -337,12 +356,13 @@ static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
     size_t pixels_left = length;
     size_t out_ptr_ix = 0;
 
-    while (pixels_left != 0) {
+    while (pixels_left != 0)
+    {
         out_ptr[out_ptr_ix] = (snapshot_buf[pixel_ix] << 16) + (snapshot_buf[pixel_ix + 1] << 8) + snapshot_buf[pixel_ix + 2];
 
         // go to the next pixel
         out_ptr_ix++;
-        pixel_ix+=3;
+        pixel_ix += 3;
         pixels_left--;
     }
     // and done!
