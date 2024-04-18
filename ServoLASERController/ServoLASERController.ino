9828b494b39ba65578f08a6dac175e9e4b3c7d46
DonatFortini <donatfortini2a@gmail.com>
2024-04-18 23:10:57 +0200

    add : joystick control

--- ServoLASERController/ServoLASERController.ino
+++ ServoLASERController/ServoLASERController.ino
@@ -1,18 +1,34 @@
 #include <Servo.h>
+#include <ezButton.h>
+
+#define SERVO_BOT 10
+#define SERVO_TOP 9
+
+#define VRX_PIN A0
+#define VRY_PIN A1
+#define SW_PIN 3
 
 Servo servoBot;
 Servo servoTop;
-
-const int maxStep = 4;
+ezButton button(SW_PIN);
 
 int data[2] = {90, 90};
 int last_data[2] = {90, 90};
 
+int xValue = 0;
+int yValue = 0;
+int buttonState = 0;
+
+bool joystickMode = false;
+
+void receivedData(int *data);
+void joystickControl();
+
 void setup()
 {
-  servoBot.attach(10);
-  servoTop.attach(9);
-
+  servoBot.attach(SERVO_BOT);
+  servoTop.attach(SERVO_TOP);
+  button.setDebounceTime(50);
   servoBot.write(data[0]);
   servoTop.write(data[1]);
   Serial.begin(115200);
@@ -20,33 +36,60 @@ void setup()
 
 void loop()
 {
-  receivedData(data);
+  button.loop();
+  buttonState = button.getState();
 
-  if (data[0] != last_data[0] || data[1] != last_data[1])
+  if (button.isPressed())
   {
-    servoBot.write(data[0]);
-    servoTop.write(data[1]);
-
-    last_data[0] = data[0];
-    last_data[1] = data[1];
+    joystickMode = !joystickMode;
+    delay(50);
   }
 
-  delay(100);
+  if (joystickMode)
+    joystickControl();
+  else
+  {
+    receivedData(data);
+    if (data[0] != last_data[0] || data[1] != last_data[1])
+    {
+      servoBot.write(data[0]);
+      servoTop.write(data[1]);
+
+      last_data[0] = data[0];
+      last_data[1] = data[1];
+    }
+  }
 }
 
+/**
+ * @brief      Controle de la position des servomoteurs avec un joystick
+ */
+void joystickControl()
+{
+  int xRaw = analogRead(VRX_PIN);
+  int yRaw = analogRead(VRY_PIN);
+
+  int xMapped = map(xRaw, 0, 1023, 0, 180);
+  int yMapped = map(yRaw, 0, 1023, 0, 180);
+
+  servoTop.write(xMapped);
+  servoBot.write(yMapped);
+}
 
 /**
- * @brief Fonction pour lire les données reçues par le port série
-*/
+ * @brief      Reception des données envoyées par le moniteur série
+ *
+ * @param      data  Tableau de deux entiers
+ */
 void receivedData(int *data)
 {
   if (Serial.available() > 0)
   {
     char receivedChars[50];
-    memset(receivedChars, 0, sizeof(receivedChars));                       
-    Serial.readBytesUntil('\n', receivedChars, sizeof(receivedChars) - 1); 
+    memset(receivedChars, 0, sizeof(receivedChars));
+    Serial.readBytesUntil('\n', receivedChars, sizeof(receivedChars) - 1);
     int number1, number2;
-    int fieldsRead = sscanf(receivedChars, "/%d,%d", &number1, &number2); 
+    int fieldsRead = sscanf(receivedChars, "/%d,%d", &number1, &number2);
 
     if (fieldsRead == 2)
     {
